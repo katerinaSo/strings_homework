da16c19cb3ceea17f7c0d304964128f7428dbefb
Katerina Solstad (katya sorok) <katya.solstad@gmail.com>
2020-10-02 12:58:40 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,46 @@
+#String Play Homework. Numbers marked with '**' are for extra challenge
+
+
+'''   
+   0. Ask the user to type a word, sentence or a favorite quote.
+   
+   1.Find the length of the user's input string
+     (put it into the variable)
+   
+   2.Print user's input string backwards.
+   
+   3.Print every other letter or space in the quote.
+   
+   4.Print quote in uppercase letters. (**extra: print in lower case)
+   
+   5.**Make the first letter in the quote the same as the last letter
+       Print it out (do not forget to put it in the new variable)
+      
+   6. Count vowels in the quote, print to the user the information
+      on vowels count for each vowel.
+   
+   7.**Add vowels counts together for total of all vowels in the quote print to the user the infor
+     
+   8. Capitalize every word in the quote, make it a title like.
+   
+   9.**Make two new strings: one is the first half of the quote
+       and another is the second half.
+       print them in reverse order second_half + first_half
+     
+'''
+
+#help:
+''' # len(str)
+    # str.capitalize()
+    # str.title()
+    # str.find()
+    # str.count(sub)
+    # str.upper()
+    # str.replace(a,b) a with b
+    # str[0], str[1],str[2]....str[len(str)-1]
+    # str[start:end(not included):step]
+    # str[0:len(str)] the whole string
+                      because split does not include the end.
+    # str[::-1] try to have negatives steps through whole string
+    
+'''
